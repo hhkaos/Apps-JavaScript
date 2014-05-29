72fb908866abaf24d9b9fecbcb5c7de73df74af0
RaulJimenezOrtega <hhkaos@gmail.com>
2014-05-29 21:44:41 +0200

    Mejorando un poco la interfaz, o eso creo xD

--- GPX_GoProHero/config.js
+++ GPX_GoProHero/config.js
@@ -1,4 +1,6 @@
 var videoId = "PQuWhPpH3Eo";
 var routeURI = "routes/20140529_granada.gpx";
-var mapType = "topo";
-var mapCenter = [-37.177787, -3.601431]
+var mapType = "streets";
+var mapCenter = [-37.177787, -3.601431];
+var iconMarker = "images/rounded_marker.png";
+var zoomLevel = 12;
